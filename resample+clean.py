0b18b51c789df48a5d10e3de5bfd78c161b68bea
Phaztox <antonin.gabet@efrei.net>
2026-01-28 16:07:02 +0100

    start resampling TH & T2

--- resample+clean.py
+++ resample+clean.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from scipy.signal import resample, resample_poly
 import matplotlib.pyplot as plt
 
-def resample_and_clean_data(input_h5_file, offsetAD1=0, offsetAD2=0, offsetP1=0, offsetP2=0, output_dir=None):
+def resample_and_clean_data(input_h5_file, offset1=0, offset2=0, offsetP1=0, offsetP2=0, output_dir=None):
     input_filename = Path(input_h5_file).stem  # Gets the filename without extension
     output_filename = f"Resampled_{input_filename}.h5"
     with h5py.File(input_h5_file, 'r') as h5f:
@@ -24,9 +24,13 @@ def resample_and_clean_data(input_h5_file, offsetAD1=0, offsetAD2=0, offsetP1=0,
             # Get the data
             data = h5f[key][:]
             if key=='AD_NAVIGATION':
-                datasets[key] = data[offsetAD1:len(data) - offsetAD2]
+                datasets[key] = data[offset1:len(data) - offset2]
             elif key=='Pressures':
                 datasets[key]=data[offsetP1:len(data) - offsetP2]
+            elif key=='TH':
+                datasets[key]=data[offset1:len(data) - offset2]
+            elif key=='T2':
+                datasets[key]=data[offset1:len(data) - offset2]
             
             # Try to get column names for this specific dataset
             label_key = f'{key}_label' if f'{key}_label' in h5f.attrs else f'{key}_LABEL'
@@ -59,9 +63,9 @@ def resample_and_clean_data(input_h5_file, offsetAD1=0, offsetAD2=0, offsetP1=0,
         time_adnav_tes = np.linspace(t_start, t_end, num_samples_adnav)
         
         # Linear interpolation for each column
-        resampled_ADnav_25to100 = np.zeros((num_samples_adnav, ADnav_short.shape[1]))
+        Resampled_ADnav_25to100 = np.zeros((num_samples_adnav, ADnav_short.shape[1]))
         for col in range(ADnav_short.shape[1]):
-            resampled_ADnav_25to100[:, col] = np.interp(time_adnav_tes, time_original, ADnav_short[:, col])
+            Resampled_ADnav_25to100[:, col] = np.interp(time_adnav_tes, time_original, ADnav_short[:, col])
 
         # ============ Resample Pressures (1000Hz -> 100Hz) ============
         Pressures_1000hz = datasets['Pressures'][:,11:26]
@@ -82,12 +86,36 @@ def resample_and_clean_data(input_h5_file, offsetAD1=0, offsetAD2=0, offsetP1=0,
         Resampled_Pressures=np.zeros((num_samples_p, Pressures_100hz.shape[1]))
         for col in range(Pressures_100hz.shape[1]):
             Resampled_Pressures[:, col]=np.interp(time_p, time_original_p, Pressures_100hz[:, col])
+
+        # ============ Resample Temparature Data ============
+        # Resample TH
+        TH_100hz=np.zeros((len(datasets['TH']), 3))
+        for i in range(len(datasets['TH'])):
+            TH_100hz[i, 0]=datasets['TH'][i, 5] 
+            TH_100hz[i, 1]=datasets['TH'][i, 6] 
+            TH_100hz[i, -1]=datasets['TH'][i, -1]  
+        time_TH=np.linspace(TH_100hz[0, -1], TH_100hz[-1, -1], int(TH_100hz[-1, -1]-TH_100hz[0, -1]))
+        time_original_TH=TH_100hz[:, -1]
+        Resampled_TH=np.zeros((int(TH_100hz[-1, -1]-TH_100hz[0, -1]), TH_100hz.shape[1]))
+        for col in range(TH_100hz.shape[1]):
+            Resampled_TH[:, col] = np.interp(time_TH, time_original_TH, TH_100hz[:, col])
+
+        # Resample T2
+        T2_100hz=np.zeros((len(datasets['T2']), 2))
+        for i in range(len(datasets['T2'])):
+            T2_100hz[i, 0]=datasets['T2'][i, 2]  
+            T2_100hz[i, -1]=datasets['T2'][i, -1]  
+        time_T2=np.linspace(T2_100hz[0, -1], T2_100hz[-1, -1], int(T2_100hz[-1, -1]-T2_100hz[0, -1]))
+        time_original_T2=T2_100hz[:, -1]
+        Resampled_T2=np.zeros((int(T2_100hz[-1, -1]-T2_100hz[0, -1]), T2_100hz.shape[1]))
+        for col in range(T2_100hz.shape[1]):
+            Resampled_T2[:, col] = np.interp(time_T2, time_original_T2, T2_100hz[:, col])
         
         # ============ Save to HDF5 ============
         output_path = os.path.join(output_dir, output_filename)
         with h5py.File(output_path, 'w') as h5f_out:
             # Save ADnav
-            h5f_out.create_dataset('Resampled_ADnav_25hzto100', data=resampled_ADnav_25to100, compression="gzip", compression_opts=4)
+            h5f_out.create_dataset('Resampled_ADnav_25hzto100', data=Resampled_ADnav_25to100, compression="gzip", compression_opts=4)
             h5f_out.create_dataset('time_adnav_tes', data=time_adnav_tes, compression="gzip", compression_opts=4)
             h5f_out.attrs['Resampled_ADnav_25hzto100_label'] = np.array(labels['AD_NAVIGATION'], dtype='S')
             h5f_out.attrs['time_adnav_tes_label'] = np.array(['Time'], dtype='S')
@@ -105,9 +133,18 @@ def resample_and_clean_data(input_h5_file, offsetAD1=0, offsetAD2=0, offsetP1=0,
             h5f_out.create_dataset('Pressures_100hz', data=Pressures_100hz, compression="gzip", compression_opts=4)
             h5f_out.attrs['Pressures_100hz_label'] = np.array(Resampled_Pressures_label, dtype='S')
 
-        return Pressures_100hz, Resampled_Pressures, resampled_ADnav_25to100
+            #Save TH data
+            Resampled_TH_label = ['Temp1', 'Hum1', 'Time']
+            h5f_out.create_dataset('Resampled_TH', data=Resampled_TH, compression="gzip", compression_opts=4)
+            h5f_out.attrs['Resampled_TH_label'] = np.array(Resampled_TH_label, dtype='S')
+            # Save T2 data
+            Resampled_T2_label = ['Temp2', 'Time']
+            h5f_out.create_dataset('Resampled_T2', data=Resampled_T2, compression="gzip", compression_opts=4)
+            h5f_out.attrs['Resampled_T2_label'] = np.array(Resampled_T2_label, dtype='S')
 
-a, b, c = resample_and_clean_data("C:\\Users\\Antonin\\Desktop\\Project Results\\MomentaVol5_clean_extracted.h5", offsetAD1=0, offsetAD2=0, offsetP1=0, offsetP2=0, output_dir="C:\\Users\\Antonin\\Desktop\\Project Results")
+        return Resampled_ADnav_25to100, Pressures_100hz, Resampled_Pressures, Resampled_TH, Resampled_T2
+
+Resampled_ADnav_25to100, Pressures_100hz, Resampled_Pressures, Resampled_TH, Resampled_T2 = resample_and_clean_data("C:\\Users\\Antonin\\Desktop\\Project Results\\MomentaVol5_clean_extracted.h5", offset1=0, offset2=0, offsetP1=0, offsetP2=0, output_dir="C:\\Users\\Antonin\\Desktop\\Project Results")
 
 
 with h5py.File("C:\\Users\\Antonin\\Desktop\\Project Results\\MomentaVol5_clean_extracted.h5", 'r') as h5f_test:
@@ -128,8 +165,8 @@ with h5py.File("C:\\Users\\Antonin\\Desktop\\Project Results\\MomentaVol5_clean_
 
 # simple plot for Pressure_100hz and Resampled_Pressures on the same plot
 plt.figure(figsize=(12, 6))
-plt.plot(a[:, 2], label='Pressure1 5T', linewidth=0.5, color='blue')
-plt.plot(b[:, 2], label='Resampled Pressure1 5T', linewidth=0.5, color='orange')
+plt.plot(Pressures_100hz[:, 2], label='Pressure1 5T', linewidth=0.5, color='blue')
+plt.plot(Resampled_Pressures[:, 2], label='Resampled Pressure1 5T', linewidth=0.5, color='orange')
 plt.title('Pressure1 5T - 100hz')
 plt.xlabel("Time")
 plt.ylabel("Pressure")
@@ -137,8 +174,8 @@ plt.legend()
 
 # simple plot for resampled_ADnav_25to100 roll and Pitch columns
 plt.figure(figsize=(12, 6))
-plt.plot(c[:, 15], label='Roll', linewidth=0.5, color='green')
-plt.plot(c[:, 16], label='Pitch', linewidth=0.5, color='red')
+plt.plot(Resampled_ADnav_25to100[:, 15], label='Roll', linewidth=0.5, color='green')
+plt.plot(Resampled_ADnav_25to100[:, 16], label='Pitch', linewidth=0.5, color='red')
 plt.title('Resampled ADnav 25Hz to 100Hz - Roll and Pitch Columns')
 plt.xlabel("Time")
 plt.ylabel("Value")
@@ -146,10 +183,19 @@ plt.legend()
 
 # simple plot for the path of the flight using resampled_ADnav_25to100 Latitude and Longitude columns
 plt.figure(figsize=(12, 6))
-plt.plot(c[:, 5], c[:, 6], label='Flight Path', linewidth=0.5, color='purple')
+plt.plot(Resampled_ADnav_25to100[:, 5], Resampled_ADnav_25to100[:, 6], label='Flight Path', linewidth=0.5, color='purple')
 plt.title('Flight Path using Latitude and Longitude')
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
+plt.legend()
+
+# simple plot for TH and T2 resampled data
+plt.figure(figsize=(12, 6))
+plt.plot(Resampled_TH[:, 0], label='Resampled TH Temp1', linewidth=0.5, color='brown')
+plt.plot(Resampled_T2[:, 0], label='Resampled T2 Temp2', linewidth=0.5, color='cyan')
+plt.title('Resampled TH and T2 Temperature Data')
+plt.xlabel("Time")
+plt.ylabel("Temperature")
 plt.legend()
 
 plt.show()
